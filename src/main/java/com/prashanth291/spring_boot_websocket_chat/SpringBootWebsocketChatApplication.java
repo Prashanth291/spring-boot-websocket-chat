7f61804c5e0ff6c0f19f39c6c231c2037e62beff
unknown <prashanthbollinedi2910@gmail.com>
2026-03-30 23:43:05 +0530

    Initial Commit

---
+++ src/main/java/com/prashanth291/spring_boot_websocket_chat/SpringBootWebsocketChatApplication.java
@@ -0,0 +1,13 @@
+package com.prashanth291.spring_boot_websocket_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebsocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prashanth291/spring_boot_websocket_chat/SpringBootWebsocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashanth291.spring_boot_websocket_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
